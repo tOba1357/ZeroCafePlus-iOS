acbcac8a0c7c302828e0bb97b11d7bd7744f600f
Kento Takemoto <kento.takemoto.ap@gmail.com>
2015-12-27 20:51:29 +0900

    get tag,date and processing closure

--- ZeroCafePlus-iOS/ZeroCafePlus-iOS/ZeroCafePlus-iOS/FirstViewController.swift
+++ ZeroCafePlus-iOS/ZeroCafePlus-iOS/ZeroCafePlus-iOS/FirstViewController.swift
@@ -21,61 +21,68 @@ class FirstViewController: UIViewController, EventViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //フレームを生成する
-//        let leftframe:CGRect = CGRect(x: 6, y: 6, width: 150, height: 200)
-//        let rightframe:CGRect = CGRect(x: 162, y: 6, width: 150, height: 200)
-//        let nextframe:CGRect = CGRect(x: 6, y: 218, width: 150, height: 200)
-
 
         let url = "https://zerocafe.herokuapp.com/api/v1/events"
         Alamofire.request(.GET, url)
             .responseJSON { response in
-                let json = JSON(response.result.value!)
-                debugPrint(response.result.value)
-                
-                let eventArray = json["events"].array! as Array
-                let eventLastId = eventArray.count
-                print("持ってきたもの:",eventArray)
-                
-                var myX :CGFloat = 6
-                var myY :CGFloat = 15
-                
-                for events in eventArray.enumerate(){
+                if response.result.isSuccess {
+                    print("通信成功")
                     
-                    let sideDecide = events.index % 2
-                    if sideDecide == 0 {
-                        let eve = events.element as JSON
-                        let eventID = "1234"
-                        let title = eve["event"]["title"].string! as String
-//                        let dateName = eve["event"]["start_time"].string! as String
-//                        let tagName = eve["event"]["tags"].string! as String
-                        let eventViewGenerate:EventView = EventView(frame:CGRectMake(myX,myY, 150, 200),tit: title,id:eventID)
-                        eventViewGenerate.mydelegate = self
-                        eventViewGenerate.layer.cornerRadius = 10
+                    let json = JSON(response.result.value!)
+                    debugPrint(response.result.value)
+                
+                    let eventArray = json["events"].array! as Array
+                    let eventLastId = eventArray.count
+                    print("持ってきたもの:",eventArray)
+                
+                    var myX :CGFloat = 6
+                    var myY :CGFloat = 15
+                
+                    for events in eventArray.enumerate(){
+                    
+                        let sideDecide = events.index % 2
+                        if sideDecide == 0 {
+                            let eve = events.element as JSON
+                            let eventID = "1234"
+                            let title = eve["event"]["title"].string! as String
+                            let dateName = eve["event"]["start_time"].string! as String
+                            let tagName : String? = { ()->(String) in
+                                if eve["event"]["category_tag"] == nil{
+                                    return ""
+                                }else {
+                                    return eve["event"]["category_tag"].string! as String
+                                }
+                            }()
+                            print(tagName)
+                            let eventViewGenerate:EventView = EventView(frame:CGRectMake(myX,myY, 150, 200),titleNameString: title,id:eventID, dateNameString: dateName, tagNameString: tagName!)
+                            eventViewGenerate.mydelegate = self
+                            eventViewGenerate.layer.cornerRadius = 10
                         
-//                        eventViewGenerate.addTarget(self, action: "onClickMakeButton:", forControlEvents: .TouchUpInside)
-//                        let attendTransitionVC: UIViewController = EventsAttendViewController()
-//                        attendTransitionVC.modalTransitionStyle = UIModalTransitionStyle.PartialCurl
-//                        self.presentViewController(attendTransitionVC, animated: true, completion: nil)
+                            self.scrollView.addSubview(eventViewGenerate)
+                            myX = 162
+                        }else{
                         
-                        self.scrollView.addSubview(eventViewGenerate)
-                        myX = 162
-                    }else{
+                          let eve = events.element as JSON
+                          let eventID = "1234"
+                          let title = eve["event"]["title"].string! as String
+                          let dateName = eve["event"]["start_time"].string! as String
+                            let tagName : String? = { ()->(String) in
+                            if eve["event"]["category_tag"] == nil{
+                                return ""
+                            }else {
+                                return eve["event"]["category_tag"].string! as String
+                            }
+                          }()
+                          let eventViewGenerate:EventView = EventView(frame:CGRectMake(myX,myY, 150, 200),titleNameString: title,id: eventID, dateNameString: dateName, tagNameString: tagName!)
+                          eventViewGenerate.mydelegate = self
+                          eventViewGenerate.layer.cornerRadius = 10
+                          self.scrollView.addSubview(eventViewGenerate)
                         
-                        let eve = events.element as JSON
-                        let eventID = "1234"
-                        let title = eve["event"]["title"].string! as String
-                        let eventViewGenerate:EventView = EventView(frame:CGRectMake(myX,myY, 150, 200),tit: title,id: eventID)
-                        eventViewGenerate.mydelegate = self
-                        eventViewGenerate.layer.cornerRadius = 10
-                        self.scrollView.addSubview(eventViewGenerate)
-                        
-                        myX = 6
-                        myY += 212
-                    }
+                          myX = 6
+                          myY += 212
+                        }
     
-                }
+                    }
                 
 //                titleName.text = eventArray[0]["event"]["title"].string
 //                dateName.text = eventArray[0]["event"]["start_time"].string
@@ -85,6 +92,9 @@ class FirstViewController: UIViewController, EventViewDelegate {
 //                titleName.textAlignment = NSTextAlignment.Center
 //                tagName.numberOfLines = 2
 //                tagName.font = UIFont.systemFontOfSize(12)
+                }else {
+                    print("通信失敗")
+                }
         }
         
     }
@@ -110,7 +120,7 @@ class EventView :UIView{
     var mydelegate: EventViewDelegate!
     var myEventID :String!
     
-    init(frame: CGRect, tit: String, id:String) {
+    init(frame: CGRect, titleNameString: String, id:String, dateNameString: String, tagNameString: String) {
         super.init(frame: frame)
         myEventID = id
         self.backgroundColor = UIColor.whiteColor()
@@ -121,9 +131,9 @@ class EventView :UIView{
         let tagName: UILabel = UILabel(frame: CGRectMake(10,120,130,50))
         let touchButton: UIButton = UIButton(frame: CGRectMake(10,10,130,180))
         
-        titleName.text = tit
-        dateName.text = ""
-        tagName.text = ""
+        titleName.text = titleNameString
+        dateName.text = dateNameString
+        tagName.text = tagNameString
         
         touchButton.setTitle("", forState: .Normal)
         touchButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
