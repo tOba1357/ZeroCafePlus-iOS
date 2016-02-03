286e92235fff79a308115da43850e45d98e3358e
peetam175 <hayapi068@gmail.com>
2016-02-03 18:16:06 +0900

    add mini Liblary

---
+++ ZeroCafePlus-iOS/ZeroCafePlus-iOS/ZeroCafePlus-iOS/CommonFunction.swift
@@ -0,0 +1,33 @@
+//
+//  CommonFunction.swift
+//  ZeroCafePlus-iOS
+//
+//  Created by Shohei_Hayashi on 2016/02/03.
+//  Copyright © 2016年 Smartphone.apps.project. All rights reserved.
+//
+
+import UIKit
+
+class CommonFunction: AnyObject {
+    
+    // imageName : "pngの名前", w or h :サイズ
+    func resizingImage(imageName imageName :String, w:Int, h:Int) ->UIImage
+    {
+        let image = UIImage(named: imageName)
+        let size = CGSize(width: w, height: h)
+        UIGraphicsBeginImageContext(size)
+        image.drawInRect(CGRectMake(0, 0, size.width, size.height))
+        var resizeImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return resizeImage
+    }
+    // rgbValue = 色のやつ
+    func UIColorFromRGB(rgbValue rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
